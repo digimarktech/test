da7035ef729577cd2d97a61d2a834d9921732bb2
Marc Aupont <mewsicmasta@gmail.com>
2021-11-04 19:45:41 -0400

    Initial Commit

---
+++ Curliosity12345/Curliosity12345App.swift
@@ -0,0 +1,17 @@
+//
+//  Curliosity12345App.swift
+//  Curliosity12345
+//
+//  Created by Marc Aupont on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Curliosity12345App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
